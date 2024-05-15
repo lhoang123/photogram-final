9a1187c34e06c606cd6cd756f0e3f9bfe89580af
lhoang123 <164918769+lhoang123@users.noreply.github.com>
2024-05-15 04:05:19 +0000

    set up user controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  def index
+    matching_users = User.all
+
+    @list_of_users = matching_users.order({ :username => :desc })
+
+    render({ :template => "users/index" })
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -80,7 +80,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  root to: "users#index"
+  
+  root "users#index"
 
 end
